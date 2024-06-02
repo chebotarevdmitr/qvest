5c070f7f552daf34dcf3967ee974850cd24247ff
DIM <4ebotarov_dim@inbox.ru>
2024-06-02 23:40:14 +0300

    вывод 1

--- hello_qvest/hello_qvest.cpp
+++ hello_qvest/hello_qvest.cpp
@@ -1,9 +1,11 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
 
 int main()
 {
-    std::cout << " \tHello qvest!\n";
-
+    std::cout <<" \tHello qvest!\n";
+    std::vector<int> v(22);
+    bool b =(v[6]);
+    printf("%d",!b);
     return 0;
 }
