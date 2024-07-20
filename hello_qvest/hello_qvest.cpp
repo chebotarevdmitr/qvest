53e822dc93e9677f2d18e3062b39f31a5bc18995
DIM <4ebotarov_dim@inbox.ru>
2024-07-20 21:25:18 +0300

    210

--- hello_qvest/hello_qvest.cpp
+++ hello_qvest/hello_qvest.cpp
@@ -1,29 +1,10 @@
 ﻿#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
-
-struct Person{
-  string name;
-  string surname;
-  int age;		
-};
-
-vector<Person> GetMoscowPopulation() {
-    vector<Person> moscowPopulation = {
-        { "Ivan", "Ivanov", 28 },
-        { "Petr", "Petrov", 30 }
-    };
-    return moscowPopulation;
-}
- 
-void PrintPopulationSize(const vector<Person>& p){
-    cout << "There are " << p.size() << " people in Moscow " << endl;
-}
 
 int main() {
-    vector<Person> people = GetMoscowPopulation();
-    PrintPopulationSize(people);
+ int x =3;
+ while(x-->0) {
+ 	std::cout<<x;
+  }  
     system("pause>0");
     return 0;
 }
